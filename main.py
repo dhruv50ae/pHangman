a991404b7ae79c10ea9e67374d898bb815b0e903
dhruv50ae <linkdp2012@gmail.com>
2022-04-09 13:53:49 +0100

    Added underscores to the guessed letters

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ wordList = ['ardvark', 'baboon', 'camel']
 chosenWord = random.choice(wordList)
 
 print(f"The chosen word is {chosenWord}")
+
+display = []
+for letter in chosenWord:
+    display += "_"
+print(display)
+
 guess = input("Guess a letter: ").lower()
 
-for letter in chosenWord:
+for position in range(len(chosenWord)):
+    letter = chosenWord[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+print(display)
