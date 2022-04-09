4328c24e67f39302aa75bb5d46f02ad6eb070006
dhruv50ae <linkdp2012@gmail.com>
2022-04-09 13:45:31 +0100

    Created the foundations of hangman

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import random
+
 wordList = ['ardvark', 'baboon', 'camel']
+
+chosenWord = random.choice(wordList)
+
+print(f"The chosen word is {chosenWord}")
+guess = input("Guess a letter: ").lower()
+
+for letter in chosenWord:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
